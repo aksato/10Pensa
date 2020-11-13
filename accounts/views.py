3f8c31e3dffcac0675f90836c45b9bb8c7025b06
Andre Sato <andre.kubagawa@usp.br>
2020-11-13 11:06:05 -0300

    Listar produtos por usuário

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views import generic
 from .forms import UserCreationFormWithEmail
+from pagina_inicial.models import Produto
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationFormWithEmail
     success_url = reverse_lazy('login')
     template_name = 'accounts/signup.html'
 
+class PerfilView(LoginRequiredMixin, generic.ListView):
+    model = Produto
+    template_name = 'accounts/perfil.html'
 
-def perfil(request):
-    return render(request, 'accounts/perfil.html')
+    def get_queryset(self):
+        return Produto.objects.filter(usuario=self.request.user)
